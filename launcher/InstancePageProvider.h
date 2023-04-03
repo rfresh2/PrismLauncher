fd8fbed3d3eac1360eec3375a688b2e40fe9262f
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-04-03 16:20:31 -0700

    Disable other logs page to fix lag with large instance folders

--- launcher/InstancePageProvider.h
+++ launcher/InstancePageProvider.h
@@ -46,7 +46,6 @@ class InstancePageProvider : protected QObject, public BasePageProvider {
         // values.append(new GameOptionsPage(onesix.get()));
         values.append(new ScreenshotsPage(FS::PathCombine(onesix->gameRoot(), "screenshots")));
         values.append(new InstanceSettingsPage(onesix));
-        values.append(new OtherLogsPage("logs", tr("Other logs"), "Other-Logs", inst));
         return values;
     }
 
